7b2ee2b858a4c82fc8ae5d2a721183b71538f5a6
stamousse <105515933+stamousse@users.noreply.github.com>
2022-05-21 12:29:40 +0200

    Update pipong.py

--- pipong.py
+++ pipong.py
@@ -17,19 +17,19 @@ class Player(GameSprite):
 	def update_r(self):
 		keys = key.get_pressed()
 		if keys[K_UP] and self.rect.y > 5:
-			self.rect.y -self.speed
+			self.rect.y -= self.speed
 		if keys[K_DOWN] and self .rect.y < win_height - 80:
 			self.rect.y += self.speed
 	def update_l(self):
 		keys = key.get_pressed()
 		if keys[K_w] and self.rect.y > 5:
-			self.rect.y -self.speed
+			self.rect.y -= self.speed
 		if keys[K_s] and self .rect.y < win_height - 80:
 			self.rect.y += self.speed
 
-background = (0, 153, 153)
-win_width = 1200
-win_height = 1000
+background = (255, 255, 255)
+win_width = 700
+win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(background)
 
@@ -38,9 +38,9 @@ finish = False
 clock = time.Clock()
 FPS = 60
 
-racket_1 = Player('racket.png', 60, 400, 8, 100, 300)
-racket_2 = Player('racket.png', 1400, 400, 8, 100, 300)
-ball = GameSprite('square.png', 540, 400, 8, 100, 100)
+racket_1 = Player('racket.png', 40, 200, 4, 50, 100)
+racket_2 = Player('racket.png', 625, 200, 4, 50, 100)
+ball = GameSprite('square.png', 350, 200, 4, 25, 25)
 
 font.init()
 font = font.Font(None, 35)
@@ -49,3 +49,36 @@ win2 = font.render('PLAYER 2 WINS!!!', True, (0, 204, 0))
 
 speed_x = 3
 speed_y = 3
+
+while game:
+	for e in event.get():
+		if e.type == QUIT:
+			game = False
+	if finish !=  True:
+		window.fill(background)
+		racket_1.update_l()
+		racket_2.update_r()
+		ball.rect.x += speed_x
+		ball.rect.y += speed_y
+	
+		if sprite.collide_rect(racket_1, ball) or sprite.collide_rect(racket_2, ball):
+			speed_x *= -1
+			speed_y *= 1
+
+		if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+			speed_y *= -1
+
+		if ball.rect.x < 0:
+			finish = True
+			window.blit(win2, (250, 200))
+		
+		if ball.rect.x > 700:
+			finish = True
+			window.blit(win1, (250, 200))
+
+	racket_1.reset()
+	racket_2.reset()
+	ball.reset()
+
+	display.update()
+	clock.tick(FPS)
